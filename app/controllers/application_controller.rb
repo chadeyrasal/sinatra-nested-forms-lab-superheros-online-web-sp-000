7255ccfcb51ac259689ac4057f77683a077505cc
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-02-04 12:19:18 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
     end
 
     post '/teams' do
-      @team = Team.new(params[team])
+      @team = Team.new(params[:team])
       params[team][members].each do |superhero_details|
         Superhero.new(superhero_details)
       end
